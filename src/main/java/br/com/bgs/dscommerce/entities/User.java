ad8c8e6f4a629e1ab35079b82f8bcf3df7d1d2e5
gustavogarciaps <sousagustavogarcia@gmail.com>
2024-01-22 17:20:31 -0300

    first commit

---
+++ src/main/java/br/com/bgs/dscommerce/entities/User.java
@@ -0,0 +1,19 @@
+package br.com.bgs.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDate birthDate;
+
+}
